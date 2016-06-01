2d6f0db32fcbce1140aff5cd9364c7ccd13b83f3
andrewbihl <andrewbihlva@gmail.com>
2016-06-01 09:05:11 -0500

    Added delete and swipe actions

--- Deprocastrinator/ViewController.m
+++ Deprocastrinator/ViewController.m
@@ -12,9 +12,9 @@
 
 @property (weak, nonatomic) IBOutlet UITextField *textField;
 @property (weak, nonatomic) IBOutlet UITableView *tableView;
-@property BOOL isEditing;
 @property NSMutableArray* tableValues;
 @property NSMutableArray* tableColors;
+@property NSIndexPath* rightSwipeStart;
 
 @end
 
@@ -25,52 +25,65 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    for (UIGestureRecognizer* cur in self.view.gestureRecognizers){
+        cur.cancelsTouchesInView = false;
+    }
     self.tableValues = [NSMutableArray new];
     self.tableColors = [NSMutableArray new];
 
 }
-- (IBAction)onRightSwipe:(UISwipeGestureRecognizer*)sender {
-    NSLog(@"Swiped right");
-    UITableViewCell* swipedCell = (UITableViewCell*)sender.view;
-    NSIndexPath* swipedCellPath = [self.tableView indexPathForCell:swipedCell];
-    UIColor* currentColor = [self.tableColors objectAtIndex:swipedCellPath.row];
-    //Cycle colors if already colored.
-    if (currentColor==[UIColor greenColor])
-        [self.tableColors replaceObjectAtIndex:swipedCellPath.row withObject:[UIColor orangeColor]];
-    else if (currentColor == [UIColor orangeColor])
-        [self.tableColors replaceObjectAtIndex:swipedCellPath.row withObject:[UIColor redColor]];
-    else if (currentColor == [UIColor redColor])
-        [self.tableColors replaceObjectAtIndex:swipedCellPath.row withObject:[UIColor greenColor]];
-    
-    [self.tableView reloadData];
-    
-}
+
 
 -(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath{
     return true;
 }
 
+- (IBAction)onSwipeCell:(UIPanGestureRecognizer*)sender {
+    if (self.tableView.isEditing)
+        return;
+    if ([sender translationInView:self.tableView].x < 0)
+        return;
+    NSIndexPath* cellPath = [self.tableView indexPathForRowAtPoint:[sender locationInView:self.tableView]];
+    if (sender.state == UIGestureRecognizerStateBegan)
+        self.rightSwipeStart = cellPath;
+    else if (sender.state == UIGestureRecognizerStateEnded && cellPath==self.rightSwipeStart){
+        NSLog(@"Right-swipe completed");
+        if ([self.tableColors objectAtIndex:cellPath.row]==[UIColor greenColor])
+            [self.tableColors replaceObjectAtIndex:cellPath.row withObject:[UIColor orangeColor]];
+        else if ([self.tableColors objectAtIndex:cellPath.row]==[UIColor orangeColor])
+            [self.tableColors replaceObjectAtIndex:cellPath.row withObject:[UIColor redColor]];
+        else if ([self.tableColors objectAtIndex:cellPath.row]==[UIColor redColor])
+            [self.tableColors replaceObjectAtIndex:cellPath.row withObject:[UIColor greenColor]];
+        [self.tableView reloadData];
+    }
+    
+}
+
 -(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath{
     if (editingStyle == UITableViewCellEditingStyleDelete){
-        [self.tableValues removeObjectAtIndex:indexPath.row];
-        [self.tableColors removeObjectAtIndex:indexPath.row];
-        [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
-//        [self.tableView reloadData];
+        NSLog(@"Left-swipe completed");
+        UIAlertController* deleteConfirm = [UIAlertController alertControllerWithTitle:@"Delete Entry?" message:nil preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction* deleteEntry = [UIAlertAction actionWithTitle:@"Delete" style:UIAlertActionStyleDestructive handler:^(UIAlertAction* _Nonnull action){
+            [self.tableValues removeObjectAtIndex:indexPath.row];
+            [self.tableColors removeObjectAtIndex:indexPath.row];
+            [self.tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
+        }];
+        UIAlertAction* cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction* _Nonnull action){
+            [self.tableView setEditing:false];
+            
+        }];
+        [deleteConfirm addAction:cancelAction];
+        [deleteConfirm addAction:deleteEntry];
+        [self presentViewController:deleteConfirm animated:true completion:nil];
     }
 }
 
-
 -(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
-    if (self.isEditing){
-        [self.tableValues removeObjectAtIndex:indexPath.row];
-        [self.tableColors removeObjectAtIndex:indexPath.row];
-    }
-    else{
-        if ([self.tableColors objectAtIndex:indexPath.row]==[UIColor blackColor])
-            [self.tableColors replaceObjectAtIndex:indexPath.row withObject:[UIColor greenColor]];
-        else
-            [self.tableColors replaceObjectAtIndex:indexPath.row withObject:[UIColor blackColor]];
-    }
+
+    if ([self.tableColors objectAtIndex:indexPath.row]==[UIColor blackColor])
+        [self.tableColors replaceObjectAtIndex:indexPath.row withObject:[UIColor greenColor]];
+    else
+        [self.tableColors replaceObjectAtIndex:indexPath.row withObject:[UIColor blackColor]];
     [self.tableView reloadData];
 }
 
@@ -78,6 +91,7 @@
     UITableViewCell* cell = [self.tableView dequeueReusableCellWithIdentifier:@"CellID"];
     cell.textLabel.text = [self.tableValues objectAtIndex:indexPath.row];
     cell.textLabel.textColor = [self.tableColors objectAtIndex:indexPath.row];
+        
     return cell;
 }
 
@@ -87,8 +101,16 @@
 
 -(void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath{
     
-}
+    NSString* myString = [self.tableValues objectAtIndex:sourceIndexPath.row];
+    [self.tableValues removeObjectAtIndex:sourceIndexPath.row];
+    [self.tableValues insertObject:myString atIndex:destinationIndexPath.row];
     
+    UIColor* myColor = [self.tableColors objectAtIndex:sourceIndexPath.row];
+    [self.tableColors removeObjectAtIndex:sourceIndexPath.row];
+    [self.tableColors insertObject:myColor atIndex:destinationIndexPath.row];
+ 
+}
+
 -(NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     return self.tableValues.count;
     
@@ -103,14 +125,13 @@
 }
 - (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
     
-    [self.tableView setEditing:!self.tableView.editing];
-    if (self.isEditing){
+    if (self.tableView.isEditing){
         sender.title = @"Edit";
-        self.isEditing = false;
+        [self.tableView setEditing:false];
     }
     else{
         sender.title = @"Done";
-        self.isEditing = true;
+        [self.tableView setEditing:true];
     }
     
 }
